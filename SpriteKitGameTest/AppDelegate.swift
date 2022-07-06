0a5ef0ada79cd4c1a8818bd08dc342fd9f55f94f
wttch（王冲） <wttch@wttch.com>
2022-07-06 14:44:28 +0800

    Initial Commit

---
+++ SpriteKitGameTest/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  SpriteKitGameTest
+//
+//  Created by Wttch on 2022/7/6.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
