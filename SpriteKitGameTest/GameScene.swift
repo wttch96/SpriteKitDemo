4370bfe8b3934c9c6ecafe89a1e03f4cef2ffd70
wttch（王冲） <wttch@wttch.com>
2022-10-11 17:43:42 +0800

    init

--- SpriteKitGameTest/GameScene.swift
+++ SpriteKitGameTest/GameScene.swift
@@ -11,8 +11,10 @@ import GameplayKit
 class GameScene: SKScene {
     
     private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    private var player: SKShapeNode?
+    private var spinnyNode: SKShapeNode?
     
+        
     override func didMove(to view: SKView) {
         
         // Get label node from scene and store it for use later
@@ -22,10 +24,23 @@ class GameScene: SKScene {
             label.run(SKAction.fadeIn(withDuration: 2.0))
         }
         
+        
         // Create shape node to use during mouse interaction
         let w = (self.size.width + self.size.height) * 0.05
         self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
+        let path = CGMutablePath()
+        path.move(to: CGPoint(x: -10, y: 0))
+        path.addLine(to: CGPoint(x: 10, y: 0))
+        path.addLine(to: CGPoint(x: 0, y: 40))
+        path.addLine(to: CGPoint(x: 0, y: 200))
+        path.addLine(to: CGPoint(x: 0, y: 40))
+        path.addLine(to: CGPoint(x: -10, y: 0))
+        self.player = SKShapeNode.init(path: path)
+        if let player = self.player {
+            player.lineWidth = 2
+            player.fillColor = .systemPink
+            self.addChild(player)
+        }
         if let spinnyNode = self.spinnyNode {
             spinnyNode.lineWidth = 2.5
             
@@ -34,6 +49,8 @@ class GameScene: SKScene {
                                               SKAction.fadeOut(withDuration: 0.5),
                                               SKAction.removeFromParent()]))
         }
+        
+        self.camera = self.childNode(withName: "//mainCamera") as? SKCameraNode
     }
     
     
@@ -43,12 +60,25 @@ class GameScene: SKScene {
             n.strokeColor = SKColor.green
             self.addChild(n)
         }
+        if let player = self.player {
+            let dx = (pos.x - player.position.x)
+            let dy = (pos.y - player.position.y)
+            let angle = atan(dy / dx)
+            print(dx, dy, dy / dx, angle)
+            if dx > 0 {
+                var a = angle - .pi / 2
+                player.run(SKAction.sequence([SKAction.rotate(toAngle: a, duration: 1, shortestUnitArc: true)]))
+            } else {
+                var a = angle + .pi / 2
+                player.run(SKAction.sequence([SKAction.rotate(toAngle: angle + .pi / 2, duration: 1, shortestUnitArc: true)]))
+            }
+        }
     }
     
     func touchMoved(toPoint pos : CGPoint) {
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
-            n.strokeColor = SKColor.blue
+            n.strokeColor = SKColor.systemPink
             self.addChild(n)
         }
     }
@@ -83,9 +113,18 @@ class GameScene: SKScene {
             print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
         }
     }
+    override func keyUp(with event: NSEvent) {
+        
+    }
     
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+//        if let player = self.player {
+//            player.position = CGPoint(x: player.position.x + 0.1, y: player.position.y + 0.2)
+//        }
+//        if let camera = self.camera {
+//            camera.position = CGPoint(x: camera.position.x + 0.1, y: camera.position.y + 0.2)
+//        }
     }
 }
